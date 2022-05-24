12a3022b2f7c26e8f90c5cf5d390a1fe7b665130
gudiino <gudino1387@gmail.com>
2022-05-24 19:03:33 -0300

    finalizacion de ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿Calculadora calc=new Calculadora();
 int opc=10;
 double x;
+Console.WriteLine("Seleccione el tipo de operacion que desea realizar y encadenar");
 while(opc!=0){
-    Console.WriteLine("Seleccione el tipo de operacion que desea realizar y encadenar");
+    Console.WriteLine("..........................");
     Console.WriteLine("1-> SUMAR");
     Console.WriteLine("2-> RESTAR");
     Console.WriteLine("3-> MULTIPLICAR"); 
@@ -10,29 +11,38 @@ while(opc!=0){
     Console.WriteLine("5-> LIMPIAR");
     Console.Write("Seleccion -> ");
     opc = Convert.ToInt32(Console.ReadLine());
-    if(opc!=5){
-        Console.WriteLine("Ingrese un número"); 
-        x = Convert.ToDouble(Console.ReadLine());
-    }
-    // Console.WriteLine("Ingrese un número"); 
-    // x = Convert.ToDouble(Console.ReadLine());
     double resultado;
-    
     switch (opc)
     {
         case 1: 
+            Console.WriteLine("..........................");
+            Console.WriteLine("SUMAR");
+            Console.Write("Ingrese un número -> "); 
+            x = Convert.ToDouble(Console.ReadLine());
             resultado = calc.Sumar(x);
             Console.WriteLine("Resultado con Suma: "+ resultado);
             break;
-        case 2: 
+        case 2:
+            Console.WriteLine(".........................."); 
+            Console.WriteLine("RESTAR");
+            Console.Write("Ingrese un número -> "); 
+            x = Convert.ToDouble(Console.ReadLine());
             resultado = calc.Restar(x);
             Console.WriteLine("Resultado con Resta: "+ resultado);
             break;
-        case 3: 
+        case 3:
+            Console.WriteLine("..........................");
+            Console.WriteLine("MULTIPLICAR");
+            Console.Write("Ingrese un número -> "); 
+            x = Convert.ToDouble(Console.ReadLine());
             resultado = calc.Multiplicar(x);
             Console.WriteLine("Resultado con Multiplicación: "+ resultado);
             break;
         case 4:
+            Console.WriteLine("..........................");
+            Console.WriteLine("DIVIDIR");
+            Console.Write("Ingrese un número -> "); 
+            x = Convert.ToDouble(Console.ReadLine());
             if(x!=0){
                 resultado = calc.Dividir(x);
                 Console.WriteLine("Resultado con División: "+ Math.Round(resultado,2));
@@ -40,13 +50,18 @@ while(opc!=0){
                 Console.WriteLine("ERROR: Ingrese un valor distinto a 0");
             }            
             break;
-        case 5: 
+        case 5:
+            Console.WriteLine("..........................");
+            Console.WriteLine("LIMPIAR");
             calc.Limpiar();
             Console.WriteLine("Memoria borrada.");
             break;
     }
-    Console.WriteLine("¿Desea realizar otro calculo?");
+    Console.WriteLine("................................");
+    Console.WriteLine("¿Desea realizar otro operación?");
     Console.WriteLine("0-> NO");
-    Console.WriteLine("1-> SI"); 
+    Console.WriteLine("1-> SI");
+    Console.Write("Seleccion -> ");
     opc = Convert.ToInt32(Console.ReadLine());
 }
+Console.WriteLine("FIN.");
